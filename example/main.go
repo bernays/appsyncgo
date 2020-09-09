b1fa70822077b3da716a1ca73681bd6aa9f5aa2a
bernays <bernays@hey.com>
2020-09-09 11:10:31 +0000

    fix linting more errors

--- example/main.go
+++ example/main.go
@@ -16,15 +16,15 @@ var logger = logrus.New()
 func init() {
 	logger.SetLevel(logrus.DebugLevel)
 }
-func main() (err error) {
+func main() {
 	logger.Error("started")
 	client, err := appsync.CreateClient("https://whom3blq6vhxhd6rkt3offziva.appsync-api.us-east-2.amazonaws.com/graphql", "default")
 	if err != nil {
 		logger.Error(err)
-		return err
 	}
 	defer func() {
 		err = client.CloseConnection(false, false)
+		logger.Error(err)
 
 	}()
 	err = client.StartConnection()
